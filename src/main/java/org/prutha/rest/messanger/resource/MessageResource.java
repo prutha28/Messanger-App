d2b0ba602818040f1fea2185bf555ef36b714928
prutha28 <prutha.bits@gmail.com>
2016-09-20 02:42:21 -0400

    Added the service call.

--- src/main/java/org/prutha/rest/messanger/resource/MessageResource.java
+++ src/main/java/org/prutha/rest/messanger/resource/MessageResource.java
@@ -1,16 +1,23 @@
 package org.prutha.rest.messanger.resource;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.prutha.rest.messanger.model.Message;
+import org.prutha.rest.messanger.service.MessageService;
+
 @Path("/messages")
 public class MessageResource {
 
+	MessageService service = new MessageService();
+	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getMessages(){
-		return "Hello World!";
+	public List<Message> getMessages(){
+		return service.getAllMessages();
 	}
 }
