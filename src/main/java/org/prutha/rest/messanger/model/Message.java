b48d9cd485056016f773beab757fdf3fa6638aa3
Prutha Sinai Khandeparker <prutha@Pruthas-MacBook-Pro.local>
2016-09-20 02:24:48 -0400

    Created the model for Message.

---
+++ src/main/java/org/prutha/rest/messanger/model/Message.java
@@ -0,0 +1,55 @@
+package org.prutha.rest.messanger.model;
+
+import java.util.Date;
+
+public class Message {
+
+	private int id;
+	private String message;
+	private String author;
+	private Date date;
+
+	public Message() {
+
+	}
+
+	public Message(int id, String message, String author) {
+		super();
+		this.id = id;
+		this.message = message;
+		this.author = author;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+}
