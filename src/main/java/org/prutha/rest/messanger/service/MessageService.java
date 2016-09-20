68e992cd3b0e93775874b3adc47de8022e6a17e6
prutha28 <prutha.bits@gmail.com>
2016-09-20 02:36:01 -0400

    Stub method for fetching dummy messages.

--- src/main/java/org/prutha/rest/messanger/service/MessageService.java
+++ src/main/java/org/prutha/rest/messanger/service/MessageService.java
@@ -1,5 +1,18 @@
 package org.prutha.rest.messanger.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.prutha.rest.messanger.model.Message;
+
 public class MessageService {
 
+	public List<Message> getAllMessages(){
+		Message m1 = new Message(1, "Winter is comin' !", "ned");
+		Message m2 = new Message(2, "Valar Morghulis!", "arya");
+		List<Message> list = new ArrayList<>();
+		list.add(m1);
+		list.add(m2);
+		return list;
+	}
 }
