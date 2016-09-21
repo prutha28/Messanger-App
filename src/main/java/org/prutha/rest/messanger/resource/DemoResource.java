5dfdac66c71bc2b0b3dd80659d88cf8a2b105574
prutha28 <prutha.bits@gmail.com>
2016-09-21 19:20:35 -0400

    Accessing params using context.

--- src/main/java/org/prutha/rest/messanger/resource/DemoResource.java
+++ src/main/java/org/prutha/rest/messanger/resource/DemoResource.java
@@ -7,7 +7,10 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 /**
  * This is a simple demo class for demoing the various other annnotations. e.g.
@@ -26,7 +29,7 @@ public class DemoResource {
 
 	@GET
 	@Path("/annotations")
-	public String getDemo(@MatrixParam("param") String matrixParam, 
+	public String getInfoUsingAnnotations(@MatrixParam("param") String matrixParam, 
 						  @HeaderParam("customHeaderName") String header,
 						  @CookieParam("cookieName") String cookie) {
 		return "Matrix Param : " + matrixParam 
@@ -34,4 +37,24 @@ public class DemoResource {
 				+ "\nCookie value : " + cookie;
 	}
 
+	
+	/**
+	 *   The problem with the above annotations is what to do when you 
+	 *   have too many headers or too many matrix params. The method definition
+	 *   would get really tedious. Plus you will need to know all the names for 
+	 *   each of the parameters. An alternative to this is using Context and 
+	 *   BeanParam annotations.
+	 *   
+	 *   While annotations like MatrixParam, HeaderParam and CookieParam can 
+	 *   be applied to any primitive datatype, the @Context annotation can only
+	 *   be applied to specific object types, one of them being, the UriInfo class 
+	 */
+	@GET
+	@Path("/context")
+	public String getInfoUsingContext( @Context UriInfo uriInfo, 
+									   @Context HttpHeaders headers){
+		return "Path:" + uriInfo.getAbsolutePath() + "\n" +
+				"Headers:" + headers.getMediaType().toString() + "\n" +
+				"Cookies:" + headers.getCookies().toString();
+	}
 }
